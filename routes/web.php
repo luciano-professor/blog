2ea6fccb39f9df59bbd5b5ab9f69665c91ce57e3
Luciano Souza <luciano.souza@ifnmg.edu.br>
2019-04-03 20:45:29 -0300

    Aula 03/04/19

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@posts');
 
 //Chamando o controllador
 Route::get('/sobre', 'PaginaBasicaController@sobre');
