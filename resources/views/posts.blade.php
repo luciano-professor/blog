61a47d5307660b468432949c0948ac21a9e54cb2
Luciano Souza <luciano.souza@ifnmg.edu.br>
2019-04-15 20:18:26 -0300

    Codigo Aula 15/04

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,25 @@
-@include('cabecalho')
 
-    <div class="container">
+@extends('principal')
 
-        <h2>Posts</h2>
-
-
-        <table class="table table-dark">
-            <tr>
-                <th>Id</th>
-                <th>Título</th>
-                <th>Texto</th>
-                <th>Data</th>
-                <th>Opções</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Teste</td>
-                <td>Lorem ipsum dolor sit amet consectetur.</td>
-                <td>19/09/2001</td>
-                <td>Editar Excluir</td>
-            </tr>
-        </table>
-
-    </div>
-@include('rodape')
+@section('conteudo-principal')
+{{-- Vai pegar o conteudo que estiver dentro da
+    section e jogar dentro do yield
+--}}
+<h2>Posts</h2>
+<table class="table table-dark">
+    <tr>
+        <th>Id</th>
+        <th>Título</th>
+        <th>Texto</th>
+        <th>Data</th>
+        <th>Opções</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>Teste</td>
+        <td>Lorem ipsum dolor sit amet consectetur.</td>
+        <td>19/09/2001</td>
+        <td>Editar Excluir</td>
+    </tr>
+</table>
+@endsection
